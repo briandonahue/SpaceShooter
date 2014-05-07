5397fb47ba8bb2020dc57dde3043cb0f18d72d1c
Brian Donahue <Brian@Brians-MacBook-Pro.local>
2014-05-07 19:47:49 -0400

    Initial commit

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+
+	public float speed;
+
+	void Start () {
+
+
+		rigidbody.velocity = transform.forward * speed;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
